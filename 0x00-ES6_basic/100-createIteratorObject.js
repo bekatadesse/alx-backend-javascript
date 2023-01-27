514efa0c332cff3813ee63795c5241ba400f1965
bekatadesse <bekatadesse5@gmail.com>
2023-01-27 23:37:41 +0300

    Create 100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,17 @@
+export default function createIteratorObject(report) {
+
+  return (function* _() {
+
+    for (const department of Object.values(report.allEmployees)) {
+
+      for (const employee of department) {
+
+        yield employee;
+
+      }
+
+    }
+
+  }());
+
+}
